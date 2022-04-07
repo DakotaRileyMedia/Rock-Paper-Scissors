31d7cc89dcfda34e8f07a3c1429ed9be4603852e
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-07 01:40:45 -0400

    Initial Commit

---
+++ Rock, Paper, Scissors/Rock__Paper__ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Paper__ScissorsApp.swift
+//  Rock, Paper, Scissors
+//
+//  Created by Dakota Riley on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Paper__ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
